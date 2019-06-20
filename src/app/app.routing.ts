e6582e377f870d5776ba85ef2ce487044ed1fb39
Emerson Joel Rojas Soliz <joel.rojas.soliz@gmail.com>
2019-06-20 14:38:29 -0400

    fix: Remove path from app's routing config file.

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,9 +3,6 @@ import { TwPostsComponent } from './container/tw-posts/tw-posts.component';
 
 export const routes: Routes = [
   {
-    path: '', pathMatch: 'full', redirectTo: '/twitter-posts-app'
-  },
-  {
-    path: 'twitter-posts-app', component: TwPostsComponent
+    path: '/', component: TwPostsComponent
   }
 ];
